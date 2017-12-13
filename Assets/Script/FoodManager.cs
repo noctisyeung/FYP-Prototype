91063b9f9fed4ba3136404feeb27d3fdba4c9450
noctisyeung <yeungkeihong@gmail.com>
2017-12-13 23:55:56 +0800

    Created GoogleCardboard environment

---
+++ Assets/Script/FoodManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void GetTheFood()
+    {
+        transform.position += new Vector3(0f, 1f, 0f);
+    }
+}
