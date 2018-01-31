b510ad0d7bf778b290c7e86a364c4bf3d9a16e56
hang1038 <31604301+hang1038@users.noreply.github.com>
2018-01-31 11:40:59 +0800

    fix

--- Assets/Script/Customer/CustomerSpawn.cs
+++ Assets/Script/Customer/CustomerSpawn.cs
@@ -6,54 +6,53 @@ using UnityEngine.UI;
 public class CustomerSpawn : MonoBehaviour
 {
 
-	public List<GameObject> customers;
-	public GameObject bubble;
-	public RecipeManager recipeManager;
-	public GameObject currentCustomer;
+    public List<GameObject> customers;
+    public GameObject bubble;
+    public RecipeManager recipeManager;
+    public GameObject currentCustomer;
     public GameObject fire;
-	private Text dishText;
-	private AudioManager audioManager;
+    private Text dishText;
+    private AudioManager audioManager;
 
-	[HideInInspector]
-	public string chosenDish;
-	public int totalCustomer;
-	public int minSpawnWait;
-	public int maxSpawnWait;
-	public int customerServed = 0;
+    [HideInInspector]
+    public string chosenDish;
+    public int totalCustomer;
+    public int minSpawnWait;
+    public int maxSpawnWait;
+    public int customerServed = 0;
 
-	public bool killMode;
+    public bool killMode;
 
-	void Start()
-	{
-		//audioManager = FindObjectOfType<AudioManager>();
-		dishText = bubble.GetComponentInChildren<Text>();
-		StartCoroutine(spawnCustomer());
-	}
+    void Start()
+    {
+        //audioManager = FindObjectOfType<AudioManager>();
+        dishText = bubble.GetComponentInChildren<Text>();
+        StartCoroutine(spawnCustomer());
+    }
 
-	void Update()
-	{
-		if (killMode)
-		{
-			killMode = false;
+    void Update()
+    {
+        if (killMode)
+        {
+            killMode = false;
 
-			if (currentCustomer)
-				destroyCustomer();
-		}
-	}
+            if (currentCustomer)
+                destroyCustomer();
+        }
+    }
 
-	IEnumerator spawnCustomer()
-	{
-		GameObject randCustomer;
-		string randRecipe;
+    IEnumerator spawnCustomer()
+    {
+        GameObject randCustomer;
+        string randRecipe;
 
-		yield return new WaitUntil(() => recipeManager.isStart);
-		List<string> recipe = new List<string>(recipeManager.chosenDish);
+        yield return new WaitUntil(() => recipeManager.isStart);
+        List<string> recipe = new List<string>(recipeManager.chosenDish);
 
-		while (true)
-		{
-			yield return new WaitUntil(() => !currentCustomer);
+        while (true)
+        {
+            yield return new WaitUntil(() => !currentCustomer);
             yield return new WaitUntil(() => !fire.activeInHierarchy);
-            yield return new WaitForSeconds(Random.Range(minSpawnWait, maxSpawnWait));
 
             if (customerServed >= totalCustomer)
             {
@@ -62,29 +61,31 @@ public class CustomerSpawn : MonoBehaviour
 
             randRecipe = recipe[Random.Range(0, recipe.Count)];
             Recipe SelectRecipe = Resources.Load<Recipe>("recipes/" + randRecipe);
-            dishText.text = "老闆整碗 "+SelectRecipe.ChineseName;                //Chi
+            dishText.text = "老闆整碗 " + SelectRecipe.ChineseName;                //Chi
             //dishText.text = "I want a "+SelectRecipe.ChineseName;                //Eng
             chosenDish = SelectRecipe.name;
-			bubble.SetActive(true);
+            bubble.SetActive(true);
 
-			if (totalCustomer - customerServed <= recipe.Count)
-				recipe.Remove(randRecipe);
+            if (totalCustomer - customerServed <= recipe.Count)
+                recipe.Remove(randRecipe);
 
-			//audioManager.Play("Walk");
-			randCustomer = customers[Random.Range(0, customers.Count)];
-			currentCustomer = Instantiate(randCustomer, transform.position, transform.rotation);
-			currentCustomer.transform.parent = gameObject.transform;
-			customers.Remove(randCustomer);
-		}
-	}
+            //audioManager.Play("Walk");
+            randCustomer = customers[Random.Range(0, customers.Count)];
+            currentCustomer = Instantiate(randCustomer, transform.position, transform.rotation);
+            currentCustomer.transform.parent = gameObject.transform;
+            customers.Remove(randCustomer);
 
-	public void destroyCustomer()
-	{
-		Destroy(currentCustomer);
-		bubble.SetActive(false);
-		currentCustomer = null;
-		customerServed++;
-	}
+            yield return new WaitForSeconds(Random.Range(minSpawnWait, maxSpawnWait));
+        }
+    }
+
+    public void destroyCustomer()
+    {
+        Destroy(currentCustomer);
+        bubble.SetActive(false);
+        currentCustomer = null;
+        customerServed++;
+    }
 
     public void hideCustomer()
     {
